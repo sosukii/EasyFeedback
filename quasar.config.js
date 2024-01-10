f8e67353a6ccff3c231d56f426ee5dbe511ea195
Svetlana Trukhina <sweta4ca@mail.ru>
2024-01-10 19:44:53 +0700

    fix: add dist dir at quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -69,7 +69,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      distDir: 'dist',
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
